41cd35fe407bfb680d79104f7c9b840d3f1de594
LeszekWrobel <leszek.wrobel@wp.eu>
2018-06-22 09:10:19 +0200

    22-06-18r01

--- zalogowany/include/modal/modal_karta_produkcji.php
+++ zalogowany/include/modal/modal_karta_produkcji.php
@@ -1,5 +1,5 @@
 <!-- Modal -->
-<div class="modal fade" id="zpiz" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="drukarki" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -9,13 +9,13 @@
         </button>
       </div>
       <div class="modal-body">
-      Wprowadzone wartości zostaną przeliczone i zapisane.<br>
-      Wybierz TAK aby utworzyć nową kartę.<br>
-      Wybierz NIE aby powrócić do edycji.
+      Wprowadzona wartości prawdopodobnie nie jest poprawna.<br>
+      <!-- Wybierz TAK aby utworzyć nową kartę.<br> -->
+      Wybierz POPRAW aby powrócić do edycji.
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" data-dismiss="modal">Nie</button>
-        <?php echo '<a href="include/karta_produkcji/zapis.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" name="submit" type="submit" value=" Zamów ">Tak</a>';?>
+        <!-- <button type="button" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" data-dismiss="modal">Nie</button> -->
+        <a href="include/karta_produkcji/zapis.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" name="submit" type="submit" value=" Zamów ">POPRAW</a>
     <?php //echo '<a href="?menuadmin=karta_produkcji&id='.$_SESSION['id'].'&id_wykrojnik='.$_SESSION['id_wykrojnik'].'" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" name="submit" type="submit" value=" Zamów ">Tak</a>';
         ?>
       </div>
--- zalogowany/include/zamowienia/include/drukarki/table_order_printers.php
+++ zalogowany/include/zamowienia/include/drukarki/table_order_printers.php
@@ -1,5 +1,5 @@
 <?php
-include 'save_table_order_printers.php'; 
+include 'save_table_order_printers.php';
 require_once "../include/connect.php";
 $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
 	if ($polaczenie->connect_errno!=0)
@@ -7,10 +7,10 @@ $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
 			echo "Error: ".$polaczenie->connect_errno;
 		}
 		else
-		{	
+		{
 ?>
 			<div class="table_order_printers_grid">
-				<?php	
+				<?php
 					echo '<div class="kod"><b>KOD</b></div>';
 					echo '<div class="klient"><b>KLIENT</b></div>';
 					echo '<div class="form"><b>WZÓR</b></div>';
@@ -20,45 +20,36 @@ $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
 					echo '<div class="reps"><b>Długość materiału</b></div>';
 					echo '<div class="termin_realizacji"><b>Termin realizacji</b></div>';
 					echo '<div class="kolory"><b>Kolory</b></div>';
-					
+
 					echo '<div class="ilosc_zebow"><b>Ilość zębów</b></div>';
 					echo '<div class="rzecz_ilosc_mat"><b>Dł.mat. rzeczywista</b></div>';
 					echo '<div class="zrealizowane"><b>Zrealizowane</b></div>';
-
-					$rezultat = @$polaczenie->query("SELECT * FROM karty_produkcji ORDER BY termin_realizacji DESC"); 
+					$rezultat = @$polaczenie->query("SELECT * FROM karty_produkcji WHERE rzeczywista_ilosc_mat = 0 ORDER BY termin_realizacji DESC");
 					while ($wiersz = $rezultat->fetch_assoc())	//tworzymy tabele zmiennych z bazy
 						{
 							echo '<div class="kod_karty_prod">';
 							include 'include/color_order_date.php'; //koloraowanie zamówien w/g data
-							echo'</div>';							
+							echo'</div>';
 							echo '<div class="nazwa_wytw"> '.$wiersz['nazwa_wytw'].'</div>';
 							echo '<div class="nazwa_wzoru"> '.$wiersz['nazwa_wzoru'].'</div>';
 							echo '<div class="ilosc_do_realizacji"> '.$wiersz['ilosc_do_realizacji'].'</div>';
 							echo '<div class="material"> '.$wiersz['material'].'</div>';
-							echo '<div class="zalecana_szer_mat"> '.$wiersz['zalecana_szer_mat'].'</div>';				
+							echo '<div class="zalecana_szer_mat"> '.$wiersz['zalecana_szer_mat'].'</div>';
 							echo '<div class="dlugosc_materialu"> '.$wiersz['dlugosc_materialu'].'</div>';
 							echo '<div class="termin_realizacji"> '.$wiersz['termin_realizacji'].'</div>';
 							echo '<div class="kolory">';
-							$kolory = explode( ",",$wiersz['kolory'] ); // tablica kolorów 
-							foreach($kolory as $value)
-								{
-									//print '<img src='.$value.'  height="15px"> ';
-									if($value!='' )
-									{	
-										print ' | '.$value;
-										//print '<img src='.$value.'  height="30px"> ';
-									}
-								}
-									print ' | ';
-							echo '</div>'; // ??????
+							$kolory = explode( ",",$wiersz['kolory'] ); // tablica kolorów
+							foreach($kolory as $value)	{	if($value!='' )	{	print ' | '.$value;	}	}
+							print ' | ';
+							echo '</div>';
 							echo '<div class="ilosc_zebow"> '.$wiersz['ilosc_zebow'].'</div>';
 							$tak = '';
 							if($wiersz['rzeczywista_ilosc_mat']!='0')
 								{
-									echo '<div class="rzecz_ilosc_mat"> '.$wiersz['rzeczywista_ilosc_mat'].'</div>'; // odczyt z bazy wpisane przez drukarza				
+									echo '<div class="rzecz_ilosc_mat"> '.$wiersz['rzeczywista_ilosc_mat'].'</div>'; // odczyt z bazy wpisane przez drukarza
 									$tak = '<img src="include/zamowienia/include/drukarki/img/checked_off.jpg" height="20px"></src>';
 								}else
-								{	
+								{
 									$tak = '<input id="submit" name="submit" type="submit" value="Tak"></input>';
 									echo '<div class="rzecz_ilosc_mat">';
 									echo '<form method="post" action="">';
@@ -70,12 +61,11 @@ $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
 									echo $tak;
 									echo '</div>';
 									echo '</form>';
-								
+
 						}
-				?>				
+				?>
 			</div>
 <?php	}
 	$polaczenie->close();
 
-?>						
-	
+?>
