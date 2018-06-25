b677ce34d9fff12997ceadc539cd50edfd8c0b3c
LeszekWrobel <leszek.wrobel@wp.eu>
2018-06-25 20:42:30 +0200

    zapisuje do bazy puste pola - błedów nie widze

--- zalogowany/include/potw_karta_prod/order_confirmation.php
+++ zalogowany/include/potw_karta_prod/order_confirmation.php
@@ -55,40 +55,63 @@ $kolory=$_SESSION['kolor'];$grafika=$_SESSION['link_img'];$uwagi=$_SESSION['comm
 }
 else
 {
+	require_once "../include/connect.php";
+	$polaczenie = @new mysqli($host,$db_user,$db_password,$db_name);
+
+	$kod_karty_prod=$_SESSION['kod_karty_prod'];$nazwa_wytw=$_SESSION['nazwa_wytw'];$nazwa_wzoru=$_SESSION['nazwa_wzoru'];$kod_ean=$_SESSION['kod_ean'];$material=$_SESSION['paper'];$klej=$_SESSION['glue'];$id_wykrojnik=$_SESSION['id_wykrojnik'];$ilosc_zebow=$_SESSION['number_of_teeth'];$nawoj=$_SESSION['direction_roll'];
+	$quantity_er=$_SESSION['quantity_er'];$bush=$_SESSION['bush'];$roll_length=$_SESSION['roll_length'];$ilosc_rolek=$_SESSION['number_of_rolls'];$ilosc_uzytkow=$_SESSION['ilosc_uzytkow'];$zalecana_szer_mat=$_SESSION['material_width'];$dlugosc_materialu=$_SESSION['raw_material_lenght'];$ilosc_do_realizacji=$_SESSION['circulation'];$termin_realizacji=$_SESSION['date_of_completion'];
+	$kolory=$_SESSION['kolor'];$grafika=$_SESSION['link_img'];$uwagi=$_SESSION['comments_to_order'];$raport_druku=$_SESSION['print_report'];$data_dodania=$_SESSION['date_of_insertion'];$data_aktualizacji=$_SESSION['date_of_edition'];$id_autora=$_SESSION['id_autora'];$ip_autora=$_SESSION['ip_autor'];
+//	$string = implode (",",$kolory);
+if (isset($kolory) && ($kolory != ''))
+{
+	$string = implode (",",$kolory);  // zapisanie kolorów w stringu z separatorem "," jako zmienna $string
+}else{$string='';}
+//$nawoj='';
+
+	if ($polaczenie->query("INSERT INTO karty_produkcji (id,kod_karty_prod,nazwa_wytw,nazwa_wzoru,kod_ean,material,klej,id_wykrojnik,ilosc_zebow, nawoj,ilosc_er,tulejka,dl_rolki,ilosc_rolek,ilosc_uzytkow,zalecana_szer_mat,dlugosc_materialu,ilosc_do_realizacji,termin_realizacji,kolory,grafika,uwagi,raport_druku,data_dodania,data_aktualizacji,id_autora,ip_autora) VALUES (NULL,'$kod_karty_prod','$nazwa_wytw','$nazwa_wzoru','$kod_ean','$material','$klej','$id_wykrojnik','$ilosc_zebow','$nawoj','$quantity_er','$bush','$roll_length','$ilosc_rolek','$ilosc_uzytkow','$zalecana_szer_mat','$dlugosc_materialu','$ilosc_do_realizacji','$termin_realizacji','$string','$grafika','$uwagi','$raport_druku','$data_dodania','$data_aktualizacji','$id_autora','$ip_autora')"))
+	{
+		include 'include/ini_session_variables.php'; // kasowanie zmiennych do ustawień startowych
+		header('Location: ?menuadmin=zamowienia_wszystkie&yes=Zamówienie karty o kodzie "'.$kod_karty_prod.'" zostało dodane.');
+
+	}
+	else
+	{
+		throw new Exception($polaczenie->error);
+	}
 $error ='Brak wszystkich danych do zapisania zamówienia';
-include $katalogskr.'/include/error.html.php';
-print $_SESSION['kod_karty_prod'].' - kod_karty_prod <br />';
-print $_SESSION['nazwa_wytw'].' - nazwa_wytw <br />';
-print $_SESSION['nazwa_wzoru'].' - nazwa_wzoru <br />';
-print $_SESSION['kod_ean'].' - kod_ean <br />';
-print $_SESSION['link_img'] = $link.'/'.$katalog.'/img/etygrafika-logo1.png <br />';
-print $_SESSION['paper'].' - paper <br />';
-print $_SESSION['glue'].' - glue <br />';
-print $_SESSION['quantity_er'].' - quantity_er <br />';
-print $_SESSION['bush'].' - bush <br />';
-print $_SESSION['ilosc_uzytkow'].' - ilosc_uzytkow <br />';
-print $_SESSION['roll_length'].' - roll_length <br />';
-print $_SESSION['ilosc_kolorow'].' - ilość kolorów <br />';
-//print $_SESSION['kolor'].' - kolory <br />';
-//$_POST['kolor'].'<br />';
-print $_SESSION['direction_roll'].' - direction_roll = nawoj <br />';
-print $_SESSION['wymiar_x_od'].' - wymiar_x_od <br />';
-print $_SESSION['wymiar_x_do'].' - wymiar_x_do <br />';
-print $_SESSION['wymiar_y_do'].' - wymiar_y_do <br />';
-print $_SESSION['wymiar_y_od'].' - wymiar_x_od <br />';
-print $_SESSION['circulation'].' - circulation <br />';
-print $_SESSION['comments_to_order'].' - comments_to_order <br />';
-print $_SESSION['termin'].' - termin <br />';
-print $_SESSION['dimension_x'].' - dimension_x <br />';
-print $_SESSION['dimension_y'].' - dimension_y <br />';
-print $_SESSION['form'].' - form <br />';
-print $_SESSION['raw_material'].' - raw_material <br />';
-print $_SESSION['number_of_teeth'].' - number_of_teeth <br />';
-print $_SESSION['uzytkow'].' - uzytkow <br />';
-print $_SESSION['reps'].' - reps <br />';
-print $_SESSION['radius'].' - radius <br />';
-print $_SESSION['date_of_completion'].' - date_of_completion <br />';
-print $_SESSION['raw_material_lenght'].' - raw_material <br />';
-print $_SESSION['material_width'].' - material_width <br />';
+// include $katalogskr.'/include/error.html.php';
+// print $_SESSION['kod_karty_prod'].' - kod_karty_prod <br />';
+// print $_SESSION['nazwa_wytw'].' - nazwa_wytw <br />';
+// print $_SESSION['nazwa_wzoru'].' - nazwa_wzoru <br />';
+// print $_SESSION['kod_ean'].' - kod_ean <br />';
+// print $_SESSION['link_img'] = $link.'/'.$katalog.'/img/etygrafika-logo1.png <br />';
+// print $_SESSION['paper'].' - paper <br />';
+// print $_SESSION['glue'].' - glue <br />';
+// print $_SESSION['quantity_er'].' - quantity_er <br />';
+// print $_SESSION['bush'].' - bush <br />';
+// print $_SESSION['ilosc_uzytkow'].' - ilosc_uzytkow <br />';
+// print $_SESSION['roll_length'].' - roll_length <br />';
+// print $_SESSION['ilosc_kolorow'].' - ilość kolorów <br />';
+// //print $_SESSION['kolor'].' - kolory <br />';
+// //$_POST['kolor'].'<br />';
+// print $_SESSION['direction_roll'].' - direction_roll = nawoj <br />';
+// print $_SESSION['wymiar_x_od'].' - wymiar_x_od <br />';
+// print $_SESSION['wymiar_x_do'].' - wymiar_x_do <br />';
+// print $_SESSION['wymiar_y_do'].' - wymiar_y_do <br />';
+// print $_SESSION['wymiar_y_od'].' - wymiar_x_od <br />';
+// print $_SESSION['circulation'].' - circulation <br />';
+// print $_SESSION['comments_to_order'].' - comments_to_order <br />';
+// print $_SESSION['termin'].' - termin <br />';
+// print $_SESSION['dimension_x'].' - dimension_x <br />';
+// print $_SESSION['dimension_y'].' - dimension_y <br />';
+// print $_SESSION['form'].' - form <br />';
+// print $_SESSION['raw_material'].' - raw_material <br />';
+// print $_SESSION['number_of_teeth'].' - number_of_teeth <br />';
+// print $_SESSION['uzytkow'].' - uzytkow <br />';
+// print $_SESSION['reps'].' - reps <br />';
+// print $_SESSION['radius'].' - radius <br />';
+// print $_SESSION['date_of_completion'].' - date_of_completion <br />';
+// print $_SESSION['raw_material_lenght'].' - raw_material <br />';
+// print $_SESSION['material_width'].' - material_width <br />';
 }
 ?>
--- zalogowany/include/potw_karta_prod/save_the_order.php
+++ zalogowany/include/potw_karta_prod/save_the_order.php
@@ -6,6 +6,7 @@ if (isset($_POST['submit']) && ($_POST['submit']) =='Potwierdz')
 	if ($_POST['circulation']!='') {$_SESSION['circulation'] = $_POST['circulation'];}
 	if ($_POST['date_of_completion']!='') {$_SESSION['date_of_completion'] = $_POST['date_of_completion'];}
 	if ($_POST['comments_to_order']!='') {$_SESSION['comments_to_order'] = $_POST['comments_to_order'];}
+	//include 'order_edi_conf.php';
 	include 'order_confirmation.php';
 }
 else
